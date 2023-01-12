bd6b789d7691de73456df1706c3e75a7fd62901c
Petar Marinov <petar.marinov@accedia.com>
2023-01-12 13:08:41 +0200

    added a custom size prop

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,48 +1,32 @@
 import React, { ImgHTMLAttributes } from 'react';
 import { styled } from '@mui/system';
-import { makeStyles } from '@mui/material';
 
 interface ImageDetails {
   imageCustomise?: ImgHTMLAttributes<HTMLImageElement>;
+  size?: 'extra-small' | 'small' | 'medium' | 'large' | 'extra-large';
 }
 
-const size = {
-  xsmall: {
-    height: 50,
-    width: 50,
-  },
+const sizes = {
+  'extra-small': 50,
+  small: 100,
+  medium: 250,
+  large: 400,
+  'extra-large': 700,
 };
 
-const useStyles = makeStyles({
-  overrides: {
-    height: ({ height }: ImgHTMLAttributes<HTMLImageElement>) =>
-      size.xsmall.height,
-    width: ({ width }: ImgHTMLAttributes<HTMLImageElement>) =>
-      size.xsmall.width,
-  },
-});
+const StyledImage = styled('img')({});
 
-const StyledImage = styled('img')({
-  variant: [
-    {
-      prop: { size: 'xsmall' },
-      style: {
-        width: '50px',
-        height: '50px',
-      },
-    },
-    {
-      prop: { size: 'small' },
-      style: {
-        width: '100px',
-        height: '100px',
-      },
-    },
-  ],
-});
+function ImageComponent({ imageCustomise, size = 'large' }: ImageDetails) {
+  const currentSize = sizes[size];
 
-function ImageComponent({ imageCustomise }: ImageDetails) {
-  return <StyledImage {...imageCustomise} alt="Something" />;
+  return (
+    <StyledImage
+      alt="Image"
+      {...imageCustomise}
+      width={currentSize}
+      height={currentSize}
+    />
+  );
 }
 
 export default ImageComponent;
