4ed22bc27592437d32a23587fbefea67492dd02f
Petar Marinov <petar.marinov@accedia.com>
2023-01-12 17:49:45 +0200

    added condition to check if src is passed or url is valid

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,5 +1,6 @@
 import React, { ImgHTMLAttributes } from 'react';
 import { styled } from '@mui/system';
+import { Container } from '@mui/material';
 
 interface ImageDetails {
   imageCustomise?: ImgHTMLAttributes<HTMLImageElement>;
@@ -19,14 +20,48 @@ const StyledImage = styled('img')(({ avatar }: ImageDetails) => ({
   borderRadius: avatar ? '50%' : 0,
 }));
 
+const StyledContainer = styled(Container)({
+  backgroundColor: 'grey',
+});
+
 function ImageComponent({
   imageCustomise,
   size = 'large',
   avatar,
 }: ImageDetails) {
   const currentSize = sizes[size];
+  const url = imageCustomise?.src;
 
-  return (
+  async function exists(url: any) {
+    let valid;
+    const result = await fetch(url, { method: 'HEAD' });
+    if (result.status === 200) {
+      valid = true;
+    } else {
+      valid = false;
+    }
+    return valid;
+  }
+
+  function isValidURL(url: any) {
+    let isValid: boolean;
+    if (
+      /^(http(s):\/\/.)[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)$/g.test(
+        url,
+      )
+    ) {
+      isValid = true;
+    } else {
+      isValid = false;
+    }
+    return isValid;
+  }
+
+  return imageCustomise?.src === undefined ||
+    !isValidURL(url) ||
+    !exists(url) ? (
+    <StyledContainer>This image could now be loaded</StyledContainer>
+  ) : (
     <StyledImage
       alt="Image"
       {...imageCustomise}
