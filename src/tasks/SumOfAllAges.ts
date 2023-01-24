982df3ff6c8fa9387cafa4f9e40272f62281f8dd
Petar Marinov <petar.marinov@accedia.com>
2023-01-25 01:07:41 +0200

    fixed name for sum of ages

--- src/tasks/SumOfAllAges.ts
+++ src/tasks/SumOfAllAges.ts
@@ -193,13 +193,13 @@ const data = [
   },
 ];
 
-function SumOfAll() {
+function findSumOfAll() {
   const sum = data
     .filter((person) => person.age <= 30)
     .reduce((sum, person) => sum + person.age, 0);
   return sum;
 }
 
-console.log(SumOfAll());
+console.log(findSumOfAll());
 
 export default {};
